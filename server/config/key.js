29f378b6f52a925e781d04e6316ef193a9084569
jCool10 <valentinohoang1908@gmail.com>
2024-05-20 12:39:28 +0700

    chore: Add server configuration files and dependencies

---
+++ server/config/key.js
@@ -0,0 +1,6 @@
+const mongoURI = {
+  admin:
+    "mongodb://rtos:rtos@ac-ysdczab-shard-00-00.dt3zhlp.mongodb.net:27017,ac-ysdczab-shard-00-01.dt3zhlp.mongodb.net:27017,ac-ysdczab-shard-00-02.dt3zhlp.mongodb.net:27017/?authSource=admin&replicaSet=atlas-sqacrn-shard-0&retryWrites=true&appName=RTOS&ssl=true",
+};
+
+module.exports = mongoURI;
